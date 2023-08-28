1d9450efb94c635ae3c44f011e73c10d4ca62a28
oleksandr.kostian <kostyansasha@ukr.net>
2023-08-28 10:28:03 +0200

    Adding Map and some points

--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -3,17 +3,18 @@ import { Text, View, StyleSheet } from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
 import { Button } from 'react-native-elements/dist/buttons/Button';
 import Spacer from '../components/Spacer';
+import { SafeAreaView } from 'react-navigation';
 
 const AccountScreen = () => {
     const { signout } = useContext(AuthContext);
 
 
-    return <>
+    return <SafeAreaView forceInset={{ top: 'always' }}>
         <Text style={{ fontSize: 48 }}>Account Screen</Text>
         <Spacer>
             <Button title="Sign Out" onPress={signout} />
         </Spacer>
-    </>
+    </SafeAreaView>
 }
 
 const styles = StyleSheet.create({});
