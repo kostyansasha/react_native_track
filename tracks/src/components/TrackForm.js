96251e756262cb5764cd552d87b70610c22076fd
oleksandr.kostian <kostyansasha@ukr.net>
2023-10-10 08:45:37 +0200

    TrackForm modyfing

--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -1,7 +1,8 @@
-import { Button } from "react-native-elements"
-import Spacer from './Spacer';
 import React, { useContext } from 'react';
+import { Input, Button } from 'react-native-elements';
+import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
     const {
@@ -10,18 +11,30 @@ const TrackForm = () => {
         stopRecording,
         changeName
     } = useContext(LocationContext);
+    const [saveTrack] = useSaveTrack();
+
 
     return (
         <>
             <Spacer>
-                <Input value={name} onChangeText={changeName} placeholder="Enter Name" />
+                <Input
+                    value={name}
+                    onChangeText={changeName}
+                    placeholder="Enter name"
+                />
             </Spacer>
-
-            {recording
-                ? (<Button title="Stop" onPress={stopRecording} />)
-                : (<Button title="Start Recording" onPress={startRecording} />)
-            }
-
+            <Spacer>
+                {recording ? (
+                    <Button title="Stop" onPress={stopRecording} />
+                ) : (
+                    <Button title="Start Recording" onPress={startRecording} />
+                )}
+            </Spacer>
+            <Spacer>
+                {!recording && locations.length ? (
+                    <Button title="Save Recording" onPress={saveTrack} />
+                ) : null}
+            </Spacer>
         </>
     );
 }
