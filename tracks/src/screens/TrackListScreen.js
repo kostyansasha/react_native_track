0589026547b054a33761f9c4e926876ee775e64b
oleksandr.kostian <kostyansasha@ukr.net>
2023-11-07 08:45:40 +0100

    Added navigation to the TrackListScreen

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';  
 
-const TrackListScreen = () => {
-    return <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-}
+const TrackListScreen = ({ navigation }) => {
+    return (
+      <>
+        <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+        <Button
+          title="Go to Track Detail"
+          onPress={() => navigation.navigate('TrackDetail')}
+        />
+      </>
+    );
+  };  
 
 const styles = StyleSheet.create({});
 
-export default TrackListScreen;
+export default TrackListScreen;  
