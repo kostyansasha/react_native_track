ba2ffe4df16379d3f4c2335d50e1b363fa01f610
oleksandr.kostian <kostyansasha@ukr.net>
2023-12-15 12:42:53 +0100

    Add token with the interceptor

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,6 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
-    baseURL: 'http://192.168.137.1:3000'
+const instance = axios.create({
+  baseURL: 'http://192.168.137.1:3000'
 });
 
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance; 
