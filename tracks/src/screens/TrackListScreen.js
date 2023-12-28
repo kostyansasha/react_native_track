c5168a699666846d7355e141d25ce014d45fa45f
oleksandr.kostian <kostyansasha@ukr.net>
2023-12-28 17:49:08 +0100

    Added list of tracks on main screen

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';  
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { Context as TrackContext } from '../context/TrackContext';
 
 const TrackListScreen = ({ navigation }) => {
-    return (
-      <>
-        <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-        <Button
-          title="Go to Track Detail"
-          onPress={() => navigation.navigate('TrackDetail')}
-        />
-      </>
-    );
-  };  
+  const { state, fetchTracks } = useContext(TrackContext);
+
+  return (
+    <>
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={() =>
+              navigation.navigate('TrackDetail', { _id: item._id })
+            }>
+
+              <Text>{item.name}</Text>
+
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </>
+  )
+};
+
+TrackListScreen.navigationOptions = {
+  title: 'Tracks'
+}
 
 const styles = StyleSheet.create({});
-
 export default TrackListScreen;  
